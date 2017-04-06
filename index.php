b9a794bc632ed805d9f2a736cdf15a0dc653ad31
xyFryan <xuyifan96@sina.com>
2017-04-06 08:45:28 +0800

    add

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<html>
+	<head>
+		<title>
+			Ryan个人测试网站
+		</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<link rel="shortcut icon" href="#">
+		<!-- <link rel="stylesheet" type="text/css" href="css/index.css"> -->
+	</head>
+	<body class="overall" id="index-overall">
+		<div class="overall-content">
+			<div class="header" id="index-header"><H1>This is a  maintaining web.Please Wating.</H1>
+<img src="img/1.gif">
+
+<?php
+echo "今天是 " . date("Y/m/d") . "<br>";
+echo "今天是 " . date("Y.m.d") . "<br>";
+echo "今天是 " . date("Y-m-d") . "<br>";
+echo "今天是 " . date("l");
+?>
+			</div>
+			<div class="bodyer" id="index-bodyer"></div>
+			<div class="footter" id="index-footter"></div>
+		</div>
+	</body>
+</html>
